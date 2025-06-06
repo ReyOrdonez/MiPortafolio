f39f6bec8657e4196df721dc00c12a8034c9ad30
ReyOrdonez <isaacoe89@gmail.com>
2025-06-06 15:58:00 -0600

    Refactor: % margins to vh margins

--- components/home/title.js
+++ components/home/title.js
@@ -8,14 +8,14 @@ const Title = () => {
       id="Home"
       className={`h-screen flex flex-col items-center text-center snap-center`}
     >
-      <div className="max-w-screen-sm mt-40">
+      <div className="max-w-screen-sm mt-[25vh]">
         <h1 className="text-myGreen ml-5">Hello!</h1>
         <h2 className="ml-5">My name is Rey Isaac</h2>
         <h3 className="mt-3 mb-12 ml-5">
           Code, design, interaction. This is my portfolio, where creativity
           meets technology.
         </h3>
-        <ContactButton className="ml-0 text-[1.8rem] mb-44 p-2 mt-[20%]" />
+        <ContactButton className="ml-0 text-[1.8rem] mb-44 p-2 mt-[10vh]" />
       </div>
     </section>
   );
