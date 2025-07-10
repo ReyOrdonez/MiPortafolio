d9de2acf8d66cd0ebc32d2064d59f9633ed6ee5b
ReyOrdonez <isaacoe89@gmail.com>
2025-07-09 19:49:57 -0600

    I finished button's animation

--- components/home/title.js
+++ components/home/title.js
@@ -9,7 +9,7 @@ const Title = () => {
       className={`h-screen flex flex-col items-center text-center snap-center`}
     >
       <div className="ml-5 max-w-screen-sm mt-[25vh] text-left">
-        <div className="bg-[rgb(235,235,235)] dark:bg-black">
+        <div className="bg-[rgb(235,235,235)] dark:bg-black transition-colors duration-200">
           <h1 className="inline text-myGreen ">Hello!</h1>
           <h2>My name is Rey Isaac</h2>
           <h3 className="mt-3 mb-12 ">
